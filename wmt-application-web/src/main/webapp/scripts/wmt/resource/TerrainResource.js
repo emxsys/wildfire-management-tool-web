1c4930e71b39d03fed12934b20114702009f2bd5
Bruce Schubert <bruce@emxsys.com>
2015-05-22 05:39:32 -0700

    Terrain: added new TerrainResource module.

---
+++ wmt-application-web/src/main/webapp/scripts/wmt/resource/TerrainResource.js
@@ -0,0 +1,64 @@
+/* 
+ * Copyright (c) 2015, Bruce Schubert <bruce@emxsys.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     - Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     - Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *     - Neither the name of Bruce Schubert,  nor the names of its 
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*global define, $ */
+
+define([
+    '../util/Log',
+    '../util/WmtUtil',
+    '../Wmt'],
+    function (
+        Log,
+        WmtUtil,
+        Wmt) {
+        "use strict";
+        var Terrain = {
+            /**
+             * Gets a terrain tuple that is compatible with the SurfaceFireResource.
+             *
+             * @param {String} aspect The direction the terrain slope is facing in degrees; 0 is north, 180 is south.
+             * @param {String} slope The steepness of the slope in degrees; 0 is flat, 90 is straight up.
+             * @param {String} elevation The elevation of the terrain in meters.
+             * @param {Function(TerrainTuple JSON)} callback Receives a simple terrain tuple using the .
+             */
+            terrainTuple: function (aspect, slope, elevation, callback) {
+                // TODO: assert the input values
+                var url = WmtUtil.currentDomain() + Wmt.TERRAIN_REST_SERVICE,
+                    query = "mime-type=application/json"
+                    + "&aspect=" + aspect
+                    + "&slope=" + slope
+                    + "&elevation=" + elevation;
+                console.log(url + '?' + query);
+                $.get(url, query, callback);
+            }
+        };
+        return Terrain;
+    }
+);
