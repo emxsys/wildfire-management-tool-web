12e076b3d5b22a51a80255d18bbad64cca863b53
Bruce Schubert <bruce@emxsys.com>
2015-06-06 21:11:20 -0700

    SkyBackgroundLayer: added documentation, changed color to be more "sky blue"

--- wmt-application-web/src/main/webapp/scripts/wmt/globe/SkyBackgroundLayer.js
+++ wmt-application-web/src/main/webapp/scripts/wmt/globe/SkyBackgroundLayer.js
@@ -30,32 +30,45 @@
 
 /*global define, $ */
 
+/**
+ * The SkyBackgroundLayer renders a background for the globe that varies
+ * between sky blue and dark blue based on altitude.
+ * 
+ * @param {WorldWind} WorldWind
+ * @returns {SkyBackgroundLayer}
+ * 
+ * @author Bruce Schubert
+ */
 define([
     '../../nasa/WorldWind'],
     function (
         WorldWind) {
         "use strict";
         /**
-         * 
+         * @constructor
          * @param {WorldWindow} worldWindow
          * @returns {SkyBackgroundLayer}
          */
         var SkyBackgroundLayer = function (worldWindow) {
+           
+            // Inherits from the basic Layer 
             WorldWind.Layer.call(this, "Sky");
+            
+            // Store the WorldWindow canvas for doRender 
             this.globeCanvas = $(worldWindow.canvas);
             
             this.MIN_ALT = 100000;
-            this.MAX_ALT = 2000000;
-            this.MIN_ALT_LIGHTNESS = 70;
-            this.MAX_ALT_LIGHTNESS = 20;
-            this.SKY_HUE = 240;
-            this.SKY_SATURATION = 50;
+            this.MAX_ALT = 1500000;
+            this.SKY_LIGHTNESS_FAR = 30;    // Dark Blue
+            this.SKY_LIGHTNESS_NEAR = 70;   // Sky Blue
+            this.SKY_HUE = 205;             // Sky Blue
+            this.SKY_SATURATION = 47;       // Sky Blue
             
         };
         SkyBackgroundLayer.prototype = Object.create(WorldWind.Layer.prototype);
 
         /**
-         * 
+         * Sets the background color of the canvas based on the eye position.
          * @param {DrawContext} dc
          */
         SkyBackgroundLayer.prototype.doRender = function (dc) {
@@ -66,14 +79,28 @@ define([
             this.globeCanvas.css('background-color', this.getCSSHSL(this.skyColor(eyePosition.altitude)));
         };
 
+        /**
+         * Gets the sky color base on the altitude.
+         * @param {Number} altitude Eye position altitude in meters.
+         * @returns {Object} HSV Sky color.
+         */
         SkyBackgroundLayer.prototype.skyColor = function (altitude) {
             var range = this.MAX_ALT - this.MIN_ALT,
                 value = Math.min(Math.max(altitude, this.MIN_ALT), this.MAX_ALT),
-                lightness = this.interpolate(this.MIN_ALT_LIGHTNESS, this.MAX_ALT_LIGHTNESS, range, value);
+                lightness = this.interpolate(this.SKY_LIGHTNESS_NEAR, this.SKY_LIGHTNESS_FAR, range, value);
             
             return {h: this.SKY_HUE, s: this.SKY_SATURATION, l: lightness};
         };
 
+
+        /**
+         * Returns an interpolated value between start and end, based on a count between a number of steps.
+         * @param {Number} start Value at lower end of range (steps)
+         * @param {Number} end value at upper end of range
+         * @param {Number} steps Range 
+         * @param {Number} count Value
+         * @returns {Number}
+         */
         SkyBackgroundLayer.prototype.interpolate = function (start, end, steps, count) {
             var s = start,
                 e = end,
@@ -81,6 +108,11 @@ define([
             return Math.floor(final);
         };
 
+        /**
+         * Gets a CSS hsl string from a HSL color
+         * @param {Object} hsl HSL color object
+         * @returns {String} CSS HSL string
+         */
         SkyBackgroundLayer.prototype.getCSSHSL = function (hsl) {
             // return the CSS HSL colour value
             return 'hsl(' + hsl.h + ',' + hsl.s + '%,' + hsl.l + '%)';
