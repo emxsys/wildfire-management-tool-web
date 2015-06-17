8de9784aea3587eee3a512072420d2e273e5a7f0
Bruce Schubert <bruce@emxsys.com>
2015-06-17 11:15:03 -0700

    Weather Lookouts: Work in progress on view. Re: #57.

--- wmt-application-web/src/main/webapp/scripts/wmt/view/WeatherPanel.js
+++ wmt-application-web/src/main/webapp/scripts/wmt/view/WeatherPanel.js
@@ -36,14 +36,12 @@ define([
     '../util/Log',
     '../model/WeatherLookout',
     '../util/Messenger',
-    '../../nasa/layer/RenderableLayer',
     '../Wmt',
     '../../nasa/WorldWind'],
     function (
         Log,
         WeatherLookout,
         Messenger,
-        RenderableLayer,
         Wmt,
         WorldWind) {
         "use strict";
@@ -52,10 +50,8 @@ define([
 
             this.ctrl = controller;
             this.manager = this.ctrl.model.weatherManager;
-            //this.manager.on(Wmt.EVENT_WEATHER_LOOKOUT_ADDED, this.handleWeatherAddedEvent, this);
-            //this.manager.on(Wmt.EVENT_WEATHER_LOOKOUT_REMOVED, this.handleWeatherRemovedEvent, this);
-
-
+            this.manager.on(Wmt.EVENT_WEATHER_LOOKOUT_ADDED, this.handleWeatherAddedEvent, this);
+            this.manager.on(Wmt.EVENT_WEATHER_LOOKOUT_REMOVED, this.handleWeatherRemovedEvent, this);
 
             // Get the RenderableLayer that we'll add the weathers to.
             this.weatherLayer = this.ctrl.findLayer(Wmt.WEATHERS_LAYER_NAME);
@@ -67,7 +63,7 @@ define([
 
             // Add button event handlers
             $("#createWeatherLookout").on("click", function (event) {
-                self.onCreateWeatherLookout();
+                this.ctrl.addWeatherLookoutToGlobe(new WeatherLookout());
             });
 
             this.loadExistingWeatherLookouts();
@@ -77,53 +73,27 @@ define([
         };
 
 
-        /**
-         * Handles the dropdown button click: adds the selected ICS weather type to the globe.
-         */
-        WeatherPanel.prototype.onCreateWeatherLookout = function () {
-            var lat = this.ctrl.model.viewpoint.target.latitude,
-                lon = this.ctrl.model.viewpoint.target.longitude;
-
-            // This action will fire the weather added event.
-            this.ctrl.model.weatherManager.addWeather(name, category, type, lat, lon);
-        };
 
         WeatherPanel.prototype.loadExistingWeatherLookouts = function () {
-            var weathers = this.ctrl.model.weatherManager.weathers,
+            var lookouts = this.ctrl.model.weatherManager.lookouts,
                 i,
                 max;
-            
-            for (i = 0, max = weathers.length; i < max; i++) {
-                this.handleWeatherAddedEvent(weathers[i]);
+
+            for (i = 0, max = lookouts.length; i < max; i++) {
+                this.handleWeatherAddedEvent(lookouts[i]);
             }
         };
 
 
+        WeatherPanel.prototype.handleWeatherAddedEvent = function (wxLookout) {
 
-
-        WeatherPanel.prototype.handleWeatherAddedEvent = function (weatherNode) {
-            var symbol = this.findSymbol(weatherNode.category, weatherNode.type);
-
-            // Handle if the model is initialized before this panel is initialized
             if (!this.weatherLayer) {
-                return;
-            }
-            if (!symbol) {
-                Log.error("WeatherPanel", "handleWeatherAddedEvent", "Symbol not found for weather type: " + weatherNode.type);
-                // Flag this weather... so we don't save it
-                weatherNode.invalid = true;
                 return;
             }
             try {
                 // Create the symbol on the globe
-                this.createRenderable(
-                    weatherNode.name,
-                    weatherNode.category,
-                    weatherNode.type,
-                    symbol,
-                    weatherNode.latitude,
-                    weatherNode.longitude);
-                // Update our list of weathers
+                this.createRenderable(wxLookout);
+                // Update our list of lookouts
                 this.synchronizeWeatherList();
             }
             catch (e) {
@@ -132,10 +102,10 @@ define([
         };
 
         /**
-         * Removes the given weather from the globe and the weather list.
-         * @param {Object} weatherNode
+         * Removes the given lookout from the globe and the lookout list.
+         * @param {WeatherLookout} wxLookout
          */
-        WeatherPanel.prototype.handleWeatherRemovedEvent = function (weatherNode) {
+        WeatherPanel.prototype.handleWeatherRemovedEvent = function (wxLookout) {
             var i,
                 max,
                 placename;
@@ -148,12 +118,11 @@ define([
                 for (i = 0, max = this.weatherLayer.renderables.length; i < max; i++) {
                     placename = this.weatherLayer.renderables[i];
 
-                    if (placename.displayName === weatherNode.name) {
+                    if (placename.displayName === wxLookout.name) {
                         this.weatherLayer.renderables.splice(i, 1);
                         break;
                     }
                 }
-
                 this.synchronizeWeatherList();
             }
             catch (e) {
@@ -168,14 +137,14 @@ define([
          * @param {string} action "goto", "edit", or remove
          */
         WeatherPanel.prototype.onWeatherItemClick = function (weatherName, action) {
-            var weathers = this.weatherLayer.renderables,
+            var renderables = this.weatherLayer.renderables,
                 weather,
                 position,
                 i,
                 len;
 
-            for (i = 0, len = weathers.length; i < len; i += 1) {
-                weather = weathers[i];
+            for (i = 0, len = renderables.length; i < len; i += 1) {
+                weather = renderables[i];
                 if (weather.displayName === weatherName) {
                     switch (action) {
                         case 'goto':
@@ -203,57 +172,17 @@ define([
         };
 
 
-        WeatherPanel.prototype.findSymbol = function (category, type) {
-            var symbol,
-                types,
-                i,
-                len;
-
-            // Find the selected symbol
-            switch (category) {
-                case 'ics':
-                    types = this.icsTypes;
-                    break;
-                case 'pushpin':
-                    types = this.pushpinTypes;
-                    break;
-                default:
-                    Log.error("WeatherPanel", "findSymbol", "unhandled weather category: " + category);
-                    return;
-            }
-            for (i = 0, len = types.length; i < len; i += 1) {
-                if (type === types[i].type) {
-                    symbol = types[i].symbol;
-                    break;
-                }
-            }
-            return symbol;
-        };
-
-
-        WeatherPanel.prototype.createRenderable = function (name, category, type, symbol, latitude, longitude) {
+        WeatherPanel.prototype.createRenderable = function (wxLookout) {
             var placemark,
                 attr = new WorldWind.PlacemarkAttributes(null),
                 hiliteAttr,
                 imageSource;
 
-            // Set up the common placemark attributes.
-            switch (category) {
-                case 'ics':
-                    imageSource = Wmt.IMAGE_PATH + "ics/" + symbol;
-                    attr.imageScale = 1.5;
-                    attr.imageOffset = new WorldWind.Offset(
-                        WorldWind.OFFSET_FRACTION, 0.5,
-                        WorldWind.OFFSET_FRACTION, 0.0);
-                    break;
-                case 'pushpin':
-                    imageSource = Wmt.WORLD_WIND_PATH + "images/pushpins/" + symbol;
-                    attr.imageScale = 0.8;
-                    attr.imageOffset = new WorldWind.Offset(
-                        WorldWind.OFFSET_FRACTION, 0.3,
-                        WorldWind.OFFSET_FRACTION, 0.0);
-                    break;
-            }
+            imageSource = Wmt.IMAGE_PATH + "ics/" + symbol;
+            attr.imageScale = 1.5;
+            attr.imageOffset = new WorldWind.Offset(
+                WorldWind.OFFSET_FRACTION, 0.5,
+                WorldWind.OFFSET_FRACTION, 0.0);
             attr.imageColor = WorldWind.Color.WHITE;
             attr.labelAttributes.offset = new WorldWind.Offset(
                 WorldWind.OFFSET_FRACTION, 0.5,
@@ -262,9 +191,9 @@ define([
             attr.drawLeaderLine = true;
             attr.leaderLineAttributes.outlineColor = WorldWind.Color.RED;
 
-            placemark = new WorldWind.Placemark(new WorldWind.Position(latitude, longitude, 0));
+            placemark = new WorldWind.Placemark(new WorldWind.Position(wxLookout.latitude, wxLookout.longitude, 0));
             placemark.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
-            placemark.displayName = name;
+            placemark.displayName = wxLookout.name;
 
 
 //            weather.label = "Weather " + "\n"
@@ -284,9 +213,17 @@ define([
             hiliteAttr.imageScale = attr.imageScale * 1.2;
             placemark.highlightAttributes = hiliteAttr;
 
-            // Inject OUR own weather properties 
-            placemark.weatherCategory = category;
-            placemark.weatherType = type;
+            // Add a reference to our wx lookout model object to the renderable
+            placemark.model = wxLookout;
+            
+            // Add a movable capability to the placemark that handles the move and synchronizes the model object
+            // See: SelectController
+            placemark.moveToLatLon = function(latitude, longitude) {
+                this.position.latitude  = latitude;
+                this.position.longitude = longitude;
+                this.model.latitude = latitude;
+                this.model.longitude = longitude;
+            };
 
             // Render the weather on the globe
             this.weatherLayer.addRenderable(placemark);
@@ -294,90 +231,40 @@ define([
 
 
         /**
-         * Populate the ICS weathers dropdown.
-         */
-        WeatherPanel.prototype.populateIcsWeatherDropdown = function () {
-            this.populateDropdown('#icsWeatherDropdown', this.icsTypes, Wmt.IMAGE_PATH + "ics/");
-        };
-
-
-        /**
-         * Populate the Pushpins dropdown.
-         */
-        WeatherPanel.prototype.populatePushpinDropdown = function () {
-            this.populateDropdown('#pushpinDropdown', this.pushpinTypes, Wmt.WORLD_WIND_PATH + "images/pushpins/");
-        };
-
-
-        /**
-         * Populate a dropdown with contents and configure an event handler to update the 
-         * dropdown item text upon selection.
-         * 
-         * @param {String} id Id of drop down panel, e.g, "#id".
-         * @param {Array} weatherArray Array of {name:, symbol:} objects.
-         * @param {String} imagePath Path to folder containing the images.
-         */
-        WeatherPanel.prototype.populateDropdown = function (id, weatherArray, imagePath) {
-            var $ul = $(id + ' ul'),
-                weatherItem,
-                i;
-
-            // Populate the dropdown contents with the weather names, symbols, and types
-            for (i = 0; i < weatherArray.length; i += 1) {
-                weatherItem = $('<li><a weathertype="' + weatherArray[i].type + '">' +
-                    '<span><img class="icon" src="' + imagePath + weatherArray[i].symbol + '" style="padding-right: 5px;"></span>' +
-                    weatherArray[i].name +
-                    '</a></li>');
-                $ul.append(weatherItem);
-            }
-            // Add a handler that displays the selected item's icon and text in the dropdown button
-            $ul.find('li a').click(function () {
-                var selectdItemHtml = $(this).html(),
-                    type = $(this).attr('weathertype'),
-                    $btn = $(id).find('.dropdown-toggle');
-
-                $btn.html(selectdItemHtml + '<span class="caret"></span>');
-                $btn.attr('weathertype', type);
-            });
-        };
-
-
-
-        /**
          * Synchronize the weather list with the renderables in the Weathers layer.
          */
         WeatherPanel.prototype.synchronizeWeatherList = function () {
             var self = this,
-                weathers = this.weatherLayer.renderables,
+                renderables = this.weatherLayer.renderables,
                 weatherList = $("#weatherList"), weatherItem,
-                weather,
+                placemark,
                 i,
                 len;
 
             // This preliminary implemenation does a brute force "clear and repopulate" of the list
             weatherList.children().remove();
 
-            for (i = 0, len = weathers.length; i < len; i += 1) {
-                weather = weathers[i];
+            for (i = 0, len = renderables.length; i < len; i += 1) {
+                placemark = renderables[i];
                 //weatherItem = $('<li class="list-group-item btn btn-block">' + weather.displayName + '</li>');
                 weatherItem =
                     '<div class="btn-group btn-block btn-group-sm">' +
-                    ' <button type="button" class="col-sm-8 btn btn-default mkr-goto">' + weather.displayName + '</button>' +
-                    ' <button type="button" class="col-sm-2 btn btn-default mkr-edit glyphicon glyphicon-pencil" style="top: 0" weatherName="' + weather.displayName + '"></button>' +
-                    ' <button type="button" class="col-sm-2 btn btn-default mkr-remove glyphicon glyphicon-trash" style="top: 0" weatherName="' + weather.displayName + '"></button>' +
+                    ' <button type="button" class="col-sm-8 btn btn-default wxlookout-goto">' + placemark.displayName + '</button>' +
+                    ' <button type="button" class="col-sm-2 btn btn-default wxlookout-edit glyphicon glyphicon-pencil" style="top: 0" weatherName="' + placemark.displayName + '"></button>' +
+                    ' <button type="button" class="col-sm-2 btn btn-default wxlookout-remove glyphicon glyphicon-trash" style="top: 0" weatherName="' + placemark.displayName + '"></button>' +
                     '</div>';
                 weatherList.append(weatherItem);
             }
             this.ctrl.redrawGlobe();
 
             // Add event handler to the buttons
-            weatherList.find('button.mkr-goto').on('click', function (event) {
+            weatherList.find('button.wxlookout-goto').on('click', function (event) {
                 self.onWeatherItemClick($(this).text(), "goto");
             });
-            weatherList.find('button.mkr-edit').on('click', function (event) {
+            weatherList.find('button.wxlookout-edit').on('click', function (event) {
                 self.onWeatherItemClick($(this).attr('weatherName'), "edit");
             });
-            weatherList.find('button.mkr-remove').on('click', function (event) {
+            weatherList.find('button.wxlookout-remove').on('click', function (event) {
                 self.onWeatherItemClick($(this).attr('weatherName'), "remove");
             });
         };
