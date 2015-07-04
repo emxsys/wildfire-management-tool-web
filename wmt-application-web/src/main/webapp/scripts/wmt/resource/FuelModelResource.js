f1a10e2f925eeaea11ffcb98edf067b0362cd74c
Bruce Schubert <bruce@emxsys.com>
2015-07-04 06:03:55 -0700

    Resources: Added documentation.

--- wmt-application-web/src/main/webapp/scripts/wmt/resource/FuelModelResource.js
+++ wmt-application-web/src/main/webapp/scripts/wmt/resource/FuelModelResource.js
@@ -29,7 +29,13 @@
  */
 
 /*global define, $ */
-
+/*
+ * 
+ * @param {type} Log
+ * @param {type} WmtUtil
+ * @param {type} Wmt
+ * @returns {FuelModelResource_L39.FuelModelResource}
+ */
 define([
     'wmt/util/Log',
     'wmt/util/WmtUtil',
@@ -49,17 +55,37 @@ define([
 
                 var url = WmtUtil.currentDomain() + Wmt.FUELMODELS_REST_SERVICE,
                     query = "mime-type=application/json"
-                    + "&category="+category;
+                    + "&category=" + category;
                 console.log(url + '?' + query);
                 $.get(url, query, callback);
             },
             /**
-             * 
+             * Gets a fuel model JSON object for the given fuel model identifier.
              * @param {String} modelNo Fuel model number.
-             * @param {Function([FuelModel JSON])} callback Receives an individual fuel model JSON objects.
+             * @param {Function(JSON)} callback Receives an individual fuel model JSON objects. Example:
+             * {
+             *    "modelNo":"6",
+             *    "modelCode":"#6",
+             *    "modelName":"Dormant brush, hardwood slash",
+             *    "modelGroup":"Original 13",
+             *    "dynamic":"false",
+             *    "dead1HrFuelLoad":{"type":"fuel_load:kg/m2","value":"0.33625400183829446"},
+             *    "dead10HrFuelLoad":{"type":"fuel_load:kg/m2","value":"0.5604233363971574"},
+             *    "dead100HrFuelLoad":{"type":"fuel_load:kg/m2","value":"0.4483386691177259"},
+             *    "liveHerbFuelLoad":{"type":"fuel_load:kg/m2","value":"0.0"},
+             *    "liveWoodyFuelLoad":{"type":"fuel_load:kg/m2","value":"0.0"},
+             *    "dead1HrSAVRatio":{"type":"surface_to_volume:m2/m3","value":"5741.469816272966"},
+             *    "dead10HrSAVRatio":{"type":"surface_to_volume:m2/m3","value":"357.6115485564305"},
+             *    "dead100HrSAVRatio":{"type":"surface_to_volume:m2/m3","value":"98.4251968503937"},
+             *    "liveHerbSAVRatio":{"type":"surface_to_volume:m2/m3","value":"0.0"},
+             *    "liveWoodySAVRatio":{"type":"surface_to_volume:m2/m3","value":"0.0"},
+             *    "fuelBedDepth":{"type":"fuel_depth:m","value":"0.7619999999999999","unit":"m"},
+             *    "moistureOfExtinction":{"type":"moisture_of_extinction:%","value":"25.0","unit":"%"},
+             *    "lowHeatContent":{"type":"heat_content:kJ/kg","value":"18608.0"},
+             *    "burnable":"true"
+             *};
              */
             fuelModel: function (modelNo, callback) {
-
                 var url = WmtUtil.currentDomain() + Wmt.FUELMODELS_REST_SERVICE + "/" + modelNo,
                     query = "mime-type=application/json";
                 console.log(url + '?' + query);
--- wmt-application-web/src/main/webapp/scripts/wmt/resource/SolarResource.js
+++ wmt-application-web/src/main/webapp/scripts/wmt/resource/SolarResource.js
@@ -45,7 +45,25 @@ define([
              * @param {Number} latitude
              * @param {Number} longitude
              * @param {Date} datetime
-             * @param {Function(Sunlight sunlight)} callback Receives sunlight JSON object.
+             * @param {Function(JSON)} callback Receives sunlight JSON object. JSON Example:
+             *  {
+             *      "observerTime": "2015-07-04T05:19:51.278-07:00[America/Los_Angeles]",
+             *      "observerLatitude": {"type": "Latitude", "value": "34.2", "unit": "deg"},
+             *      "observerLongitude": {"type": "Longitude", "value": "-119.2", "unit": "deg"},
+             *      "observerAltitude": {"type": "Altitude", "value": "0.0", "unit": "m"},
+             *      "subsolarLatitude": {"type": "subsolar_lat:deg", "value": "22.871266284130332", "unit": "deg"},
+             *      "subsolarLongitude": {"type": "subsolar_lon:deg", "value": "-3.8697908736235718", "unit": "deg"},
+             *      "azimuthAngle": {"type": "azimuth:deg", "value": "56.89353027163992", "unit": "deg"},
+             *      "altitudeAngle": {"type": "altitude:deg", "value": "-6.175449369022366", "unit": "deg"},
+             *      "zenithAngle": {"type": "zenith:deg", "value": "96.17544936902236", "unit": "deg"},
+             *      "localHourAngle": {"type": "hour:deg", "value": "244.66979087362358", "unit": "deg"},
+             *      "sunriseHourAngle": {"type": "sunrise_hour:deg", "value": "-107.86718940052833", "unit": "deg"},
+             *      "sunsetHourAngle": {"type": "sunset_hour:deg", "value": "107.83565447548936", "unit": "deg"},
+             *      "sunriseTime": "05:49:57-07:00",
+             *      "sunsetTime": "20:12:25-07:00",
+             *      "sunTransitTime": "13:01:13-07:00"
+             *  }
+             * 
              */
             sunlightAtLatLonTime: function (latitude, longitude, datetime, callback) {
 
@@ -56,6 +74,7 @@ define([
                     + "&longitude=" + longitude;
                 console.log(url + '?' + query);
                 $.get(url, query, callback);
+
             },
             /**
              * 
