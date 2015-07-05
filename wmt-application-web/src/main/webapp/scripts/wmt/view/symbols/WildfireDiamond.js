9b065a2d4a17457133159ac604290ee15f18afba
Bruce Schubert <bruce@emxsys.com>
2015-07-05 01:31:40 -0700

    WildfireDiamond: new map symbol for fire lookouts.  Work in progress.

---
+++ wmt-application-web/src/main/webapp/scripts/wmt/view/symbols/WildfireDiamond.js
@@ -0,0 +1,86 @@
+/* 
+ * Copyright (c) 2015, Bruce Schubert <bruce@emxsys.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     - Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     - Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *     - Neither the name of Bruce Schubert, Emxsys nor the names of its 
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*global define, WorldWind*/
+
+define([
+    'wmt/Wmt',
+    'worldwind'],
+    function (
+        wmt,
+        ww) {
+        "use strict";
+
+        var SkyCoverPlacemark = function (latitude, longitude, head, left, right, heal, eyeDistanceScaling) {
+            WorldWind.Placemark.call(this, new WorldWind.Position(latitude, longitude, wmt.MAP_SYMBOL_ALTITUDE_WILDFIRE), eyeDistanceScaling);
+
+            this.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
+
+            this.attributes = new WorldWind.PlacemarkAttributes(null);
+            this.attributes.depthTest = false;
+            this.attributes.imageScale = 0.25;
+            this.attributes.imageOffset = new WorldWind.Offset(
+                WorldWind.OFFSET_FRACTION, 0.5,
+                WorldWind.OFFSET_FRACTION, 0.5);
+            this.attributes.labelAttributes.offset = new WorldWind.Offset(
+                WorldWind.OFFSET_FRACTION, 0.5, // Centered
+                WorldWind.OFFSET_FRACTION, 2.2);    // Below RH
+            this.attributes.drawLeaderLine = true;
+            this.attributes.leaderLineAttributes.outlineColor = WorldWind.Color.RED;
+            this.attributes.leaderLineAttributes.outlineWidth = 2;
+            this.attributes.labelAttributes.color = WorldWind.Color.WHITE;
+            this.attributes.labelAttributes.depthTest = false;
+            this.highlightAttributes = new WorldWind.PlacemarkAttributes(this.attributes);
+            //this.highlightAttributes.imageScale = placemarkAttr.imageScale * 1.2;
+            //this.eyeDistanceScalingThreshold = 2500000;
+
+            this.updateWildfireDiamondImage(head, left, right, heal);
+
+        };
+        // Inherit Placemark parent methods
+        SkyCoverPlacemark.prototype = Object.create(WorldWind.Placemark.prototype);
+
+
+        SkyCoverPlacemark.prototype.updateWildfireDiamondImage = function (head, left, right, heal) {
+            var img = new Image(),
+                imgName;
+
+            if (!head || !left || !right || !heal) {
+                imgName = 'unkn';
+            }
+            imgName = 'unkn';
+            this.attributes.imageSource = wmt.IMAGE_PATH + 'fire/' + imgName + '.png';
+            this.highlightAttributes.imageSource = this.attributes.imageSource;
+        };
+
+        return SkyCoverPlacemark;
+    }
+);
+
