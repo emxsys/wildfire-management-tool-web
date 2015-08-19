55ff2ba835be95a4951d0f158306b736294f6d4d
Bruce Schubert <bruce@emxsys.com>
2015-08-19 07:06:30 -0700

    Wildfire Diamond: now renders unknown (white) diamond if the head fire is NaN. Resolves #141.

--- wmt-application-web/src/main/webapp/scripts/wmt/view/symbols/fire/WildfireDiamond.js
+++ wmt-application-web/src/main/webapp/scripts/wmt/view/symbols/fire/WildfireDiamond.js
@@ -72,7 +72,7 @@ define([
             var imgName = 'unkn';
 
             // if burnable
-            if (head) {
+            if (!isNan(head)) {
                 imgName = WildfireDiamond.getColorCode(head)
                     + WildfireDiamond.getColorCode(flanks)
                     + WildfireDiamond.getColorCode(flanks)
